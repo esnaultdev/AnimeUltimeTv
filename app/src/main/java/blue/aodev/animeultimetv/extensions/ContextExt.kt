65db1d72f1d97a4882cc94b51cb26070c6931b8c
Ao <aodevblue@gmail.com>
2017-06-30 23:30:12 +0200

    Improve duration display

--- app/src/main/java/blue/aodev/animeultimetv/extensions/ContextExt.kt
+++ app/src/main/java/blue/aodev/animeultimetv/extensions/ContextExt.kt
@@ -1,9 +1,10 @@
 package blue.aodev.animeultimetv.extensions
 
 import android.content.Context
+import blue.aodev.animeultimetv.R
 
 fun Context.formatEpisodeDuration(duration: Int): String {
     val minutes = duration / 60
     val seconds = duration % 60
-    return "$minutes:$seconds"
+    return getString(R.string.episode_duration, minutes, seconds)
 }
