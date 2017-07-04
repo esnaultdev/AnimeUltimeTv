cb1942ddda87dac6abf31962ab69a90816ab5f70
Ao <aodevblue@gmail.com>
2017-07-05 00:40:17 +0200

    Cache the seek bitmaps

--- app/src/main/java/blue/aodev/animeultimetv/presentation/playback/NetworkPlaybackSeekDataProvider.kt
+++ app/src/main/java/blue/aodev/animeultimetv/presentation/playback/NetworkPlaybackSeekDataProvider.kt
@@ -4,12 +4,15 @@ import android.graphics.Bitmap
 import android.media.MediaMetadataRetriever
 import android.support.v17.leanback.media.PlaybackGlue
 import android.support.v17.leanback.media.PlaybackTransportControlGlue
+import android.util.LruCache
 
 
 class NetworkPlaybackSeekDataProvider(duration: Long, interval: Long, videoUrl: String)
     : PlaybackSeekAsyncDataProvider() {
 
     private val metadataRetriever: MediaMetadataRetriever = MediaMetadataRetriever()
+
+    private val bitmapCache: LruCache<Int, Bitmap>
 
     init {
         val size = (duration / interval).toInt() + 1
@@ -19,14 +22,31 @@ class NetworkPlaybackSeekDataProvider(duration: Long, interval: Long, videoUrl: 
         }
         seekPositions = pos
         metadataRetriever.setDataSource(videoUrl, emptyMap())
+
+        // Use 1/8th of the available memory for this memory cache.
+        val maxMemory = (Runtime.getRuntime().maxMemory() / 1024).toInt()
+        val cacheSize = maxMemory / 8
+
+        bitmapCache = object : LruCache<Int, Bitmap>(cacheSize) {
+            override fun sizeOf(key: Int, bitmap: Bitmap): Int {
+                // The cache size will be measured in kilobytes rather than number of items.
+                return bitmap.byteCount / 1024
+            }
+        }
     }
 
     override fun doInBackground(task: Any?, index: Int, position: Long): Bitmap? {
         if (isCancelled(task)) {
             return null
         }
-        val bitmap = metadataRetriever.getFrameAtTime(position * 1000)
-        return bitmap
+        val cachedBitmap = bitmapCache.get(index)
+        if (cachedBitmap != null) {
+            return cachedBitmap
+        } else {
+            val bitmap = metadataRetriever.getFrameAtTime(position * 1000)
+            bitmapCache.put(index, bitmap)
+            return bitmap
+        }
     }
 
     companion object {
