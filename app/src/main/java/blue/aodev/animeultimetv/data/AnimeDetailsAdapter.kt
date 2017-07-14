da3b06aca856c9f22856a73e78908a82fcf067cc
Ao <aodevblue@gmail.com>
2017-07-14 16:33:26 +0200

    Fix anime info parsing for some old animes

--- app/src/main/java/blue/aodev/animeultimetv/data/AnimeDetailsAdapter.kt
+++ app/src/main/java/blue/aodev/animeultimetv/data/AnimeDetailsAdapter.kt
@@ -35,25 +35,18 @@ internal class AnimeDetailsAdapter : Converter<ResponseBody, AnimeDetails> {
         var splitDescription = titleLessDescription.split("\r\n").toMutableList()
 
         val synopsis = splitDescription
-                .takeWhile { !it.trim().startsWith("TITRE ORIGINAL :") }
+                .takeWhile { !it.trim().startsWith("TITRE ORIGINAL") }
                 .map { Jsoup.parse(it).text() } // Remove escaped characters
                 .fold(StringBuilder(), { sb, it -> sb.append("$it\n") })
                 .toString()
                 .trim(' ', '\n', '\r')
 
         splitDescription = splitDescription
-                .dropWhile { !(it.contains("ANNÉE DE PRODUCTION : ")
-                        || it.contains("ANNÉES DE PRODUCTION : ")) }
+                .dropWhile { !(it.contains("ANNÉE DE PRODUCTION")
+                        || it.contains("ANNÉES DE PRODUCTION")) }
                 .toMutableList()
 
-        val rawYears = splitDescription[0].split(" : ")[1]
-        val yearsSplit = rawYears.split(" - ")
-        val years = if (yearsSplit.size == 1) {
-            val year = parseYear(yearsSplit.first())
-            year..year
-        } else {
-            parseYear(yearsSplit.first())..parseYear(yearsSplit.last())
-        }
+        val years = parseYears(splitDescription[0])
         splitDescription.removeAt(0)
 
         val studios = getValuesInBrackets(splitDescription.first())
@@ -71,14 +64,26 @@ internal class AnimeDetailsAdapter : Converter<ResponseBody, AnimeDetails> {
         return AnimeDetails(synopsis, years, studios, genres, author)
     }
 
+    private fun parseYears(yearString: String): IntRange {
+        val rawYears = yearString.split("PRODUCTION").last().trim(' ', ':')
+        val yearsSplit = rawYears.split(" - ")
+        val years = if (yearsSplit.size == 1) {
+            val year = parseYear(yearsSplit.first())
+            year..year
+        } else {
+            parseYear(yearsSplit.first())..parseYear(yearsSplit.last())
+        }
+        return years
+    }
+
+    private fun parseYear(yearString: String): Int {
+        return yearString.trim().toInt()
+    }
+
     private fun getValuesInBrackets(input: String): List<String> {
         return bracketRegex.findAll(input)
                 .map { it.value }
                 .map { it.drop(1).dropLast(1) } // Remove the [] around the names
                 .toList()
-    }
-
-    private fun parseYear(yearString: String): Int {
-        return yearString.trim().toInt()
     }
 }
