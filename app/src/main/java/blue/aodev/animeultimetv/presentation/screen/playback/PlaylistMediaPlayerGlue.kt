cf951a0889c89b06e99dc3112dd73a81cead6321
Ao <aodevblue@gmail.com>
2017-09-03 01:14:22 +0200

    Remove the hd action in playback

--- app/src/main/java/blue/aodev/animeultimetv/presentation/screen/playback/PlaylistMediaPlayerGlue.kt
+++ app/src/main/java/blue/aodev/animeultimetv/presentation/screen/playback/PlaylistMediaPlayerGlue.kt
@@ -4,10 +4,8 @@ import android.app.Activity
 import android.net.Uri
 import android.support.v17.leanback.media.PlaybackGlue
 import android.support.v17.leanback.media.PlaybackTransportControlGlue
-import android.support.v17.leanback.widget.Action
 import android.support.v17.leanback.widget.ArrayObjectAdapter
 import android.support.v17.leanback.widget.PlaybackControlsRow
-import android.widget.Toast
 import blue.aodev.animeultimetv.domain.model.Playlist
 
 
@@ -16,7 +14,6 @@ class PlaylistMediaPlayerGlue(context: Activity, impl: ExoPlayerAdapter, var pla
 
     private val previousAction = PlaybackControlsRow.SkipPreviousAction(context)
     private val nextAction = PlaybackControlsRow.SkipNextAction(context)
-    private val hdAction = PlaybackControlsRow.HighQualityAction(context)
 
     init {
         updateCurrentVideo()
@@ -36,53 +33,6 @@ class PlaylistMediaPlayerGlue(context: Activity, impl: ExoPlayerAdapter, var pla
         adapter.add(previousAction)
         adapter.add(nextAction)
     }
-
-    override fun onCreateSecondaryActions(adapter: ArrayObjectAdapter) {
-        super.onCreateSecondaryActions(secondaryActionsAdapter)
-        adapter.add(hdAction)
-    }
-
-    override fun onActionClicked(action: Action) {
-        if (shouldDispatchAction(action)) {
-            dispatchAction(action)
-            return
-        }
-        super.onActionClicked(action)
-    }
-
-    private fun shouldDispatchAction(action: Action): Boolean {
-        return action === hdAction
-    }
-
-    private fun dispatchAction(action: Action) {
-        Toast.makeText(context, action.toString(), Toast.LENGTH_SHORT).show()
-        val multiAction = action as PlaybackControlsRow.MultiAction
-        multiAction.nextIndex()
-        notifyActionChanged(multiAction)
-    }
-
-    private fun notifyActionChanged(action: PlaybackControlsRow.MultiAction) {
-        var index = -1
-        if (primaryActionsAdapter != null) {
-            index = primaryActionsAdapter!!.indexOf(action)
-        }
-        if (index >= 0) {
-            primaryActionsAdapter!!.notifyArrayItemRangeChanged(index, 1)
-        } else {
-            if (secondaryActionsAdapter != null) {
-                index = secondaryActionsAdapter!!.indexOf(action)
-                if (index >= 0) {
-                    secondaryActionsAdapter!!.notifyArrayItemRangeChanged(index, 1)
-                }
-            }
-        }
-    }
-
-    private val primaryActionsAdapter: ArrayObjectAdapter?
-        get() = controlsRow?.primaryActionsAdapter as ArrayObjectAdapter?
-
-    private val secondaryActionsAdapter: ArrayObjectAdapter?
-        get() = controlsRow?.secondaryActionsAdapter as ArrayObjectAdapter?
 
     fun playWhenReady() {
         if (isPrepared) {
